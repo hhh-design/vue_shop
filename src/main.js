f68a1906426efb3f3622878934403091f7cbbeb6
2456944980 <abcwangjintao@163.com>
2021-06-06 20:10:53 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -5,16 +5,24 @@ import './plugins/element.js'
 import './assets/css/global.css'
 /* 导入字体图标 */
 import './assets/css/iconfont.css'
-/* 导入弹框提示组件 */
-import { Message } from 'element-ui'
+
 
 import axios from 'axios'
 /* 配置请求的根路径 */
 axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+
+/* 在挂载到原型对象之前先设置拦截器 */
+axios.interceptors.request.use(config => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  /* 最后必须返回config */
+  return config;
+})
+
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-Vue.prototype.$message = Message;
+
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,34 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Row } from 'element-ui'
-
+import {
+  Button, Form, FormItem, Input, Row, Container, Aside, Main, Header,
+  Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Col, Table, TableColumn,
+  Switch, Tooltip, Pagination, Dialog, MessageBox
+} from 'element-ui'
+/* 导入弹框提示组件 */
+import { Message } from 'element-ui'
 
 Vue.use(Button)/* 注册为全局可用的组件 */
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Row)
-
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Header)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.prototype.$message = Message;
+Vue.prototype.$confirm = MessageBox.confirm;
 
