ca42299cbd6d06b28d2b51dd61414708d41b5f71
2456944980 <abcwangjintao@163.com>
2021-06-09 11:42:07 +0800

    完成了权限功能的开发

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -74,6 +74,7 @@
                 type="warning"
                 icon="el-icon-setting"
                 size="mini"
+                @click="setRole(scope.row)"
               ></el-button>
             </el-tooltip>
           </template>
@@ -141,6 +142,35 @@
         <span slot="footer" class="dialog-footer">
           <el-button @click="editDialogVisible = false">取 消</el-button>
           <el-button type="primary" @click="editUserInfo">确 定</el-button>
+        </span>
+      </el-dialog>
+
+      <!-- 分配角色的对话框 -->
+      <el-dialog
+        title="分配角色"
+        :visible.sync="setRoleDialogVisible"
+        width="50%"
+        @close="setRoleDialogClosed"
+      >
+        <div>
+          <p>当前的用户：{{ userInfo.username }}</p>
+          <p>当前的角色：{{ userInfo.role_name }}</p>
+          <p>
+            分配新角色：
+            <el-select v-model="selectedRoleId" placeholder="请选择">
+              <el-option
+                v-for="item in rolesList"
+                :key="item.id"
+                :label="item.roleName"
+                :value="item.id"
+              >
+              </el-option>
+            </el-select>
+          </p>
+        </div>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
         </span>
       </el-dialog>
 
@@ -241,6 +271,12 @@
             { validator: checkMobile, trigger: 'blur' },
           ],
         },
+        setRoleDialogVisible: false,
+        userInfo: {},
+        /* 所有角色的数据列表 */
+        rolesList: [],
+        /* 已选中的角色id值 */
+        selectedRoleId: '',
       }
     },
     /* 页面创建期间，使用该函数 */
@@ -354,9 +390,7 @@
         ).catch((cancel) => {
           return cancel
         })
-        /* 用户确认删除会返回confirm字符串
-                                    用户取消删除会返回cancel字符串 
-                                console.log(confirmResult)*/
+        /* 用户确认删除会返回confirm字符串用户取消删除会返回cancel字符串 console.log(confirmResult)*/
         if (confirmResult == 'cancel') {
           return this.$message('已取消删除')
         }
@@ -369,6 +403,38 @@
         }
         this.$message.success('删除成功')
         this.getUserList()
+      },
+      /* 展示分配角色的对话框 */
+      async setRole(userInfo) {
+        this.userInfo = userInfo
+
+        /* 在展示对话框之前，获取所有角色的列表 */
+        const { data: res } = await this.$http.get('roles')
+        if (res.meta.status != 200) {
+          return this.$message.error('获取角色列表失败')
+        }
+
+        this.rolesList = res.data
+
+        this.setRoleDialogVisible = true
+      },
+      async saveRoleInfo() {
+        const { data: res } = await this.$http.put(
+          `users/${this.userInfo.id}/role`,
+          {
+            rid: this.selectedRoleId,
+          }
+        )
+        if (res.meta.status != 200) {
+          return this.$message.error(res.meta.msg)
+        }
+        this.$message.success('更新角色成功')
+        this.getUserList()
+        this.setRoleDialogVisible = false
+      },
+      setRoleDialogClosed() {
+        this.selectedRoleId = ''
+        this.userInfo = {}
       },
     },
   }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import {
   Button, Form, FormItem, Input, Row, Container, Aside, Main, Header,
   Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Col, Table, TableColumn,
-  Switch, Tooltip, Pagination, Dialog, MessageBox
+  Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Option, Select
 } from 'element-ui'
 /* 导入弹框提示组件 */
 import { Message } from 'element-ui'
@@ -29,6 +29,11 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Option)
+Vue.use(Select)
+
 Vue.prototype.$message = Message;
 Vue.prototype.$confirm = MessageBox.confirm;
 
